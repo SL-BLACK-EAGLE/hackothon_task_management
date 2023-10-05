73b71620c531d85936d81dba72e6aaf12e6352e9
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-10-05 09:56:57 +0530

    init tailwind and NextUi Library

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div>Navbar</div>
+    )
+}
+export default Index
---
+++ src/pages/Employee/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Employee = () => {
+    return (
+        <div>Employee</div>
+    )
+}
+export default Employee
